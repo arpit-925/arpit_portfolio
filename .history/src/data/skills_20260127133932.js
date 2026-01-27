498b012d14985654703ff5d445129b10afc00992
Arpit mishra <arpitmishra0017@gmail.com>
2026-01-27 14:08:50 +0530

    chore: remove editor history files

--- .history/src/data/skills_20260127133932.js
+++
@@ -1,6 +0,0 @@
-export const skills = {
-  frontend: ["React", "Tailwind CSS", "Redux", "React Router"],
-  backend: ["Node.js", "Express.js", "REST APIs", "Auth"],
-  database: ["MongoDB", "MySQL"],
-  tools: ["Git", "GitHub", "Vercel", "Render", "Cloudinary", "Stripe", "Mapbox"],
-};
