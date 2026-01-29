6df43c0999db4294d7a17a3926f27edb596d27c4
Arpit mishra <arpitmishra0017@gmail.com>
2026-01-29 20:16:24 +0530

    routing issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { i } from "framer-motion/client";
 import SocialBar from "./components/SocialBar";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import SectionDivider from "./components/SectionDivider";   
+
 
 const App = () => {
   return (
